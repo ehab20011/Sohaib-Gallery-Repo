527b7b3f654e17c84234f10b02c86112d2f1c449
Ehab Abdalla <121260254+ehab20011@users.noreply.github.com>
2024-02-25 12:58:49 -0500

    Work Page IPG

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/navbar';
 import Home from './components/Home';
 import Work from './components/Work'; 
 import Contact from './components/Contact';
+import Pictures from './components/Pictures'; 
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
+          <Route path="/photos" component={Pictures} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,28 @@
-// Work.js
 import React from 'react';
-import './Work.css'; // Import the CSS file
+import './Work.css'; 
+import workpicone from './workpicone.jpg';
+import workpictwo from './workpictwo.jpg';
+import workpicthree from './workpicthree.jpg';
+import WorkPic from './WorkPic.jpg'; //
 
 function Work() {
   return (
-    <div className="work-container">
-      <h1>This is the Work Page</h1>
+    <div className="work-background">
+      <img src={WorkPic} alt="Background" className="background-image" />
+      <div className="work-container">
+        <div className="image-container">
+          <img src={workpicone} alt="Work Image 1" className="work-image" />
+          <div className="overlay"><div className="text">Sports</div></div>
+        </div>
+        <div className="image-container">
+          <img src={workpictwo} alt="Work Image 2" className="work-image" />
+          <div className="overlay"><div className="text">Portraits</div></div>
+        </div>
+        <div className="image-container">
+          <img src={workpicthree} alt="Work Image 3" className="work-image" />
+          <div className="overlay"><div className="text">Automotive</div></div>
+        </div>
+      </div>
     </div>
   );
 }
